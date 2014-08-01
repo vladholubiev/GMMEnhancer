66d365a084b99b31c7600c4c836321be84821712
samosfator <golubev.vld@yandex.ru>
2014-08-01 04:54:16 +0300

    Replaced old stats bar to googles hidden one

--- js/gmm/statBar.js
+++ js/gmm/statBar.js
@@ -1,30 +1,13 @@
-var userLink = $('a[href*="edit-nickname"]').attr('href').replace('#edit-nickname', '');
-
-$.get(userLink, function(data) {
-	var draftStat = $(data).find('.time_period_large').text().match(/\d+/g);
-	var days = draftStat[0];
-	var edits = draftStat[1];
-	var reviews = draftStat[3];
-	var editsPerDay = (edits / days).toPrecision(2);
-
-	if ($('.edits:visible').length < 1) {
-		$('#gbqfbw').after('<span title class="stats reviews">' + reviews + '</span>');
-		$('#gbqfbw').after('<span title class="stats days">' + days + '</span>');
-		$('#gbqfbw').after('<span title class="stats edits">' + edits + '</span>');
-	} else {
-		$('.edits').text(edits);
-		$('.days').text(days);
-		$('.reviews').text(reviews);
-	}
-	$('.stats.edits').attr('title', editsPerDay + ' ' + chrome.i18n.getMessage('statBar_editsPerDay'));
-	$('.stats.days').attr('title', chrome.i18n.getMessage('statBar_days'));
-	$('.stats.reviews').attr('title', chrome.i18n.getMessage('statBar_reviews'));
-
-	$(document).ready(function() {
-		if ($('.letter').length < 1) {
-			$('.stats.reviews').after('<span class="letter">' + chrome.i18n.getMessage('statBar_reviewsLetter') + '</span>');
-			$('.stats.days').after('<span class="letter">' + chrome.i18n.getMessage('statBar_dayLetter') + '</span>');
-			$('.stats.edits').after('<span class="letter">' + chrome.i18n.getMessage('statBar_editsLetter') + '</span>');
-		}
-	});
+$(document).ready(function () {
+    $('#div-mini-dashboard').css({
+        display: 'block',
+        position: 'fixed',
+        bottom: '0',
+        background: '#fff',
+        padding: '9px 9px 9px 44px',
+        width: ($('#panel').width() - 44 - 9) + 'px'
+    });
+    $('#lp-control-panel').remove();
+    $('#div-mini-dashboard').show();
+    $('#spsizer').css({height: '100%'});
 });
