274279c16b82c7659aad567a0d3766c289a685dc
samosfator <golubev.vld@yandex.ru>
2014-08-19 06:43:10 +0300

    Removed debugging information

--- js/gmm/hotkeys/delete.js
+++ js/gmm/hotkeys/delete.js
@@ -4,9 +4,6 @@
 
     $(document).ready(function () {
         $(document).keydown("keydown", function (e) {
-
-            console.log("keydown");
-
             var intervalID;
             var found = false;
             intervalID = setInterval(function () {
@@ -14,9 +11,7 @@
                 var item = $('.contextmenu .menuitem').eq(0);
                 if (item.length == 1) {
                     found = true;
-                    console.log(found);
                     if (e.keyCode == 46) {
-                        console.log("handled click");
                         item.click();
                     }
                     clearInterval(intervalID);
